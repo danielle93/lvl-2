96d1896d302db1c01cf32cdc83546a20bf9ffb23
Danielle Roberson <danielle.roberson@icloud.com>
2017-04-29 20:49:41 -0700

    Level up 2!

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+// @codekit-prepend "jquery.js";
+// @codekit-prepend "semantic.js";
+
+
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.min-hand');
+const hoursHand = document.querySelector('.hour-hand');
+
+function setDate() {
+//    console.log("HENLO FREN");
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    console.log(seconds);
+    
+    
+    
+    
+    const minutes = now.getMinutes();
+    const minutesDegrees = ((minutes / 60) * 360) + 90;
+    
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    console.log(minutes);
+    
+    
+    
+    
+    const hours = now.getHours();
+    const hoursDegrees = ((hours / 60) * 360) + 90;
+    
+    hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
+    console.log(hours);
+}
+
+setInterval(setDate, 1000);
