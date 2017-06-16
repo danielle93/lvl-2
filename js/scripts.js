7bdf53c18467181df1ba33ce9e7647324d72b2cf
Danielle Roberson <danielle.roberson@icloud.com>
2017-06-16 04:54:11 -0700

    toggle

--- js/scripts.js
+++ js/scripts.js
@@ -16,38 +16,61 @@ t+=" .ui.visible."+a+".sidebar ~ .fixed, .ui.visible."+a+".sidebar ~ .pusher {  
 // @codekit-prepend "jquery.js";
 // @codekit-prepend "semantic.js";
 
+console.log('HENLO');
+// scripts
 
+
+//analog clock
+      
 const secondHand = document.querySelector('.second-hand');
-const minuteHand = document.querySelector('.min-hand');
-const hoursHand = document.querySelector('.hour-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
 
 function setDate() {
-//    console.log("HENLO FREN");
-    const now = new Date();
+  const now = new Date();
+    
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
-    
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-    console.log(seconds);
-    
-    
-    
-    
-    const minutes = now.getMinutes();
-    const minutesDegrees = ((minutes / 60) * 360) + 90;
-    
-    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
-    console.log(minutes);
-    
-    
-    
-    
-    const hours = now.getHours();
-    const hoursDegrees = ((hours / 60) * 360) + 90;
-    
-    hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
-    console.log(hours);
-}
 
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+    const hour = now.getHours();
+    const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+    
+
+  }
+      
 setInterval(setDate, 1000);
+
+/// toggle visibility
+$('body').on("click touchstart", "#toggle", function(e){
+   $("#analog, #digital").toggle();
+});
+
+
+// digital clock
+function clock() {
+var time = new Date(),
+    
+    hours = time.getHours(),
+    minutes = time.getMinutes(),
+    seconds = time.getSeconds();
+
+document.querySelectorAll('#digital')[0].innerHTML = harold(hours) + ":" + harold(minutes) + ":" + harold(seconds);
+  
+  function harold(standIn) {
+    if (standIn < 10) {
+      standIn = '0' + standIn
+    }
+    return standIn;
+  }
+}
+setInterval(clock, 1000);
+
+
 
